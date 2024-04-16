fe117618bfe7dbc6a563faed494888f06f52eacf
Tarik Kirgin <tarikkirgin@gmail.com>
2024-04-16 14:32:10 +0100

    add shadcn-ui

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,48 +1,77 @@
-const defaultTheme = require("tailwindcss/defaultTheme");
-
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: "selector",
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: ["class"],
+  content: [
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
+  prefix: "",
   theme: {
-    extend: {},
-    screens: {
-      xxxs: "300px",
-      xxs: "375px",
-      xs: "475px",
-      ...defaultTheme.screens,
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    extend: {
+      colors: {
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
     },
   },
-  plugins: [
-    function ({ addComponents, theme }) {
-      addComponents({
-        ".link": {
-          color: theme("colors.black"),
-          "border-bottom-width": "1.5px",
-          "border-color": theme("colors.neutral.200"),
-          "&:hover": {
-            "border-color": theme("colors.neutral.500"),
-            transition: "all 0.3s",
-          },
-          "&:focus": {
-            "border-color": theme("colors.neutral.500"),
-            transition: "all 0.3s",
-          },
-        },
-        ".link-dark": {
-          color: theme("colors.white"),
-          "border-bottom-width": "1.5px",
-          "border-color": theme("colors.neutral.700"),
-          "&:hover": {
-            "border-color": theme("colors.neutral.300"),
-            transition: "all 0.3s",
-          },
-          "&:focus": {
-            "border-color": theme("colors.neutral.300"),
-            transition: "all 0.3s",
-          },
-        },
-      });
-    },
-  ],
+  plugins: [require("tailwindcss-animate")],
 };
