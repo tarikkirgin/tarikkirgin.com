543db640e40a326188aa4e058032bb9d110728c1
Tarik Kirgin <tarikkirgin@gmail.com>
2024-04-16 02:39:34 +0100

    add more screen sizes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,17 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {},
+    screens: {
+      xxxs: "300px",
+      xxs: "375px",
+      xs: "475px",
+      ...defaultTheme.screens,
+    },
+  },
+  plugins: [
+  ],
+};
