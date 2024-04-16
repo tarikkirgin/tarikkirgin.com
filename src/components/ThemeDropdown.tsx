492756f3d6dd4678ee4d957e4964c1e65106ec1d
Tarik Kirgin <tarikkirgin@gmail.com>
2024-04-16 14:19:54 +0100

    add primitive dark mode

---
+++ src/components/ThemeDropdown.tsx
@@ -0,0 +1,44 @@
+// import { Button } from "~/components/ui/button";
+// import {
+//   DropdownMenu,
+//   DropdownMenuContent,
+//   DropdownMenuItem,
+//   DropdownMenuTrigger,
+// } from "~/components/ui/dropdown-menu";
+
+// import { MoonIcon, SunIcon } from "./ui/icons";
+
+export default function ThemeDropdown() {
+  const onClickLight = () => {
+    document.documentElement.classList.remove("dark");
+    localStorage.setItem("theme", "light");
+  };
+
+  const onClickDark = () => {
+    document.documentElement.classList.add("dark");
+    localStorage.setItem("theme", "dark");
+  };
+
+  //   return (
+  //     <DropdownMenu>
+  //       <DropdownMenuTrigger asChild>
+  //         <Button variant="ghost" size="icon">
+  //           <SunIcon className="h-5 w-5 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+  //           <MoonIcon className="absolute h-5 w-5 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+  //           <span className="sr-only">Toggle theme</span>
+  //         </Button>
+  //       </DropdownMenuTrigger>
+  //       <DropdownMenuContent align="end">
+  //         <DropdownMenuItem onClick={onClickLight}>Light</DropdownMenuItem>
+  //         <DropdownMenuItem onClick={onClickDark}>Dark</DropdownMenuItem>
+  //       </DropdownMenuContent>
+  //     </DropdownMenu>
+  //   );
+
+  return (
+    <div>
+      <button onClick={onClickLight}>Light</button>
+      <button onClick={onClickDark}>Dark</button>
+    </div>
+  );
+}
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,6 +2,7 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: "selector",
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {},
@@ -18,13 +19,27 @@ export default {
         ".link": {
           color: theme("colors.black"),
           "border-bottom-width": "1.5px",
-          "border-color": theme("colors.gray.200"),
-          transition: "all 0.3s ease-in-out",
+          "border-color": theme("colors.neutral.200"),
           "&:hover": {
-            "border-color": theme("colors.gray.500"),
+            "border-color": theme("colors.neutral.500"),
+            transition: "all 0.3s",
           },
           "&:focus": {
-            "border-color": theme("colors.gray.500"),
+            "border-color": theme("colors.neutral.500"),
+            transition: "all 0.3s",
+          },
+        },
+        ".link-dark": {
+          color: theme("colors.white"),
+          "border-bottom-width": "1.5px",
+          "border-color": theme("colors.neutral.700"),
+          "&:hover": {
+            "border-color": theme("colors.neutral.300"),
+            transition: "all 0.3s",
+          },
+          "&:focus": {
+            "border-color": theme("colors.neutral.300"),
+            transition: "all 0.3s",
           },
         },
       });
