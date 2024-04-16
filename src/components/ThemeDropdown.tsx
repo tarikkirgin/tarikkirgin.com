f5b011ab3dcb3abc6e6ae0bb3db60d5ab9fc5306
Tarik Kirgin <tarikkirgin@gmail.com>
2024-04-16 14:21:13 +0100

    add primitive dark mode

--- src/components/ThemeDropdown.tsx
+++ src/components/ThemeDropdown.tsx
@@ -1,13 +1,3 @@
-// import { Button } from "~/components/ui/button";
-// import {
-//   DropdownMenu,
-//   DropdownMenuContent,
-//   DropdownMenuItem,
-//   DropdownMenuTrigger,
-// } from "~/components/ui/dropdown-menu";
-
-// import { MoonIcon, SunIcon } from "./ui/icons";
-
 export default function ThemeDropdown() {
   const onClickLight = () => {
     document.documentElement.classList.remove("dark");
@@ -18,22 +8,6 @@ export default function ThemeDropdown() {
     document.documentElement.classList.add("dark");
     localStorage.setItem("theme", "dark");
   };
-
-  //   return (
-  //     <DropdownMenu>
-  //       <DropdownMenuTrigger asChild>
-  //         <Button variant="ghost" size="icon">
-  //           <SunIcon className="h-5 w-5 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-  //           <MoonIcon className="absolute h-5 w-5 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-  //           <span className="sr-only">Toggle theme</span>
-  //         </Button>
-  //       </DropdownMenuTrigger>
-  //       <DropdownMenuContent align="end">
-  //         <DropdownMenuItem onClick={onClickLight}>Light</DropdownMenuItem>
-  //         <DropdownMenuItem onClick={onClickDark}>Dark</DropdownMenuItem>
-  //       </DropdownMenuContent>
-  //     </DropdownMenu>
-  //   );
 
   return (
     <div>
