9bd5e66d169a9e249180c85798fa59d71adfb6b5
Tarik Kirgin <tarikkirgin@gmail.com>
2024-04-17 15:40:55 +0100

    update theme dropdown styling

--- src/components/ThemeDropdown.tsx
+++ src/components/ThemeDropdown.tsx
@@ -1,29 +1,24 @@
 import { useStore } from "@nanostores/react";
-import { THEME_MAP, themeStore } from "~/lib/stores/theme";
+import { THEME_MAP, type ThemeKey, themeStore } from "~/lib/stores/theme";
 
-export default function ThemeDropdown() {
+export default function ThemeDropdown(props: any) {
   const theme = useStore(themeStore);
 
+  console.log("theme: ", theme);
+
   return (
-    <div>
-      <button
-        className="justify-between"
-        onClick={() => themeStore.set(THEME_MAP.dark)}
+    <label className="group relative flex items-center">
+      <select
+        className="select-none appearance-none cursor-pointer pl-4 pr-8 py-2 bg-white hover:bg-neutral-100 focus:bg-neutral-100  dark:bg-neutral-900 dark:hover:bg-neutral-800 dark:focus:bg-neutral-800 ring-1 ring-neutral-200 dark:ring-neutral-800 text-black dark:text-white rounded-md"
+        defaultValue={theme}
+        onChange={(e) => themeStore.set(THEME_MAP[e.target.value as ThemeKey])}
       >
-        Dark
-      </button>
-      <button
-        className="justify-between"
-        onClick={() => themeStore.set(THEME_MAP.light)}
-      >
-        Light
-      </button>
-      <button
-        className="justify-between"
-        onClick={() => themeStore.set(THEME_MAP.system)}
-      >
-        System
-      </button>
-    </div>
+        {Object.keys(THEME_MAP).map((key) => {
+          const themeKey = key as ThemeKey;
+          return <option key={key}>{themeKey}</option>;
+        })}
+      </select>
+      {props.chevron}
+    </label>
   );
 }
