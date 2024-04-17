eed7447170a099cdbdac902d15dfd759f82dd458
Tarik Kirgin <tarikkirgin@gmail.com>
2024-04-17 16:03:28 +0100

    add aria-label to select

--- src/components/ThemeDropdown.tsx
+++ src/components/ThemeDropdown.tsx
@@ -5,8 +5,9 @@ export default function ThemeDropdown(props: any) {
   const theme = useStore(themeStore);
 
   return (
-    <label className="group relative flex items-center">
+    <div className="group relative flex items-center">
       <select
+        aria-label="Select theme"
         className="select-none appearance-none cursor-pointer pl-4 pr-8 py-2 bg-white hover:bg-neutral-100 focus:bg-neutral-100  dark:bg-neutral-900 dark:hover:bg-neutral-800 dark:focus:bg-neutral-800 ring-1 ring-neutral-200 dark:ring-neutral-800 text-black dark:text-white rounded-md"
         defaultValue={theme}
         onChange={(e) => themeStore.set(THEME_MAP[e.target.value as ThemeKey])}
@@ -17,6 +18,6 @@ export default function ThemeDropdown(props: any) {
         })}
       </select>
       {props.chevron}
-    </label>
+    </div>
   );
 }
