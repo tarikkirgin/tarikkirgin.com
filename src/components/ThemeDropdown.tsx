ca121783a891c7cab4b6b2888a75e0d430048929
Tarik Kirgin <tarikkirgin@gmail.com>
2024-04-22 02:11:31 +0100

    update accessibility labels

--- src/components/ThemeDropdown.tsx
+++ src/components/ThemeDropdown.tsx
@@ -7,7 +7,7 @@ export default function ThemeDropdown(props: any) {
   return (
     <div className="group relative flex items-center pl-2">
       <select
-        aria-label="Select theme"
+        title="Select theme"
         className="select-none appearance-none cursor-pointer pl-4 pr-8 py-2 bg-white hover:bg-neutral-100 focus:bg-neutral-100  dark:bg-neutral-900 dark:hover:bg-neutral-800 dark:focus:bg-neutral-800 ring-1 ring-neutral-200 dark:ring-neutral-800 text-black dark:text-white rounded-md"
         defaultValue={theme}
         onChange={(e) => themeStore.set(THEME_MAP[e.target.value as ThemeKey])}
