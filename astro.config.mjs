e8d2426521eea56f6faa1ca8d6231c83043a8606
Tarik Kirgin <hi@tarikkirgin.com>
2024-09-02 23:40:49 +0100

    add blog content collection and basic layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,7 @@ import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon(), react()],
-  output: "server",
+  output: "hybrid",
   adapter: vercel({
     webAnalytics: {
       enabled: true,
---
+++ src/content/config.ts
@@ -0,0 +1,19 @@
+import { z, defineCollection } from "astro:content";
+
+const blogCollection = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    date: z.date(),
+    author: z.string(),
+    image: z.object({
+      url: z.string(),
+      alt: z.string(),
+    }),
+    tags: z.array(z.string()),
+  }),
+});
+
+export const collections = {
+  blogs: blogCollection,
+};
