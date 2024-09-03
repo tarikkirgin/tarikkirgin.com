92d1c2e6ea745246dbc40017af3a404d2ee05cf7
Tarik Kirgin <hi@tarikkirgin.com>
2024-09-03 22:30:32 +0100

    add blog
    commit d8a70d8d5d9ea934935156fd1055d2e1cc4c4b7b
    Author: Tarik Kirgin <hi@tarikkirgin.com>
    Date:   Tue Sep 3 22:27:33 2024 +0100
    
        add blog to homepage
    
    commit 9e736cd38a616f9f8673cb9289dc7061e487d81d
    Author: Tarik Kirgin <hi@tarikkirgin.com>
    Date:   Tue Sep 3 22:27:20 2024 +0100
    
        allow style passing to heading component
    
    commit a082542cc7cc62bc120295c597beab7a358c6884
    Author: Tarik Kirgin <hi@tarikkirgin.com>
    Date:   Tue Sep 3 22:27:06 2024 +0100
    
        add blog page
    
    commit 72d6b7d6db6e422ae859d9a8fc57e3751b8bb1e2
    Author: Tarik Kirgin <hi@tarikkirgin.com>
    Date:   Tue Sep 3 22:27:00 2024 +0100
    
        add blog components
    
    commit 971f92ea737aa6c2f7984aa7aa80a0613ebe54bb
    Author: Tarik Kirgin <hi@tarikkirgin.com>
    Date:   Tue Sep 3 21:19:18 2024 +0100
    
        update markdown styling
    
    commit 0ff707b2aaefee7cac5d3025b86b30cfe0fe9633
    Author: Tarik Kirgin <hi@tarikkirgin.com>
    Date:   Tue Sep 3 21:01:23 2024 +0100
    
        disable markdown heading links
    
    commit d92e1a6aff3cd115ce7a9a1b7574531855a3078f
    Author: Tarik Kirgin <hi@tarikkirgin.com>
    Date:   Tue Sep 3 16:50:22 2024 +0100
    
        fix collection schema
    
    commit 4d4a668b8207c0710294ec1bf3404a7cabaf91b4
    Author: Tarik Kirgin <hi@tarikkirgin.com>
    Date:   Tue Sep 3 16:31:00 2024 +0100
    
        fix collection incorrect name
    
    commit 232a7ed6ecd007de211cfa6672dca95950660848
    Author: Tarik Kirgin <hi@tarikkirgin.com>
    Date:   Tue Sep 3 03:29:31 2024 +0100
    
        add footnote label styling
    
    commit 11bcbde5e67d42bb3cec5226c5aa1eee98f7d0b8
    Author: Tarik Kirgin <hi@tarikkirgin.com>
    Date:   Tue Sep 3 03:27:45 2024 +0100
    
        change focus to focus-visible in global css
    
    commit 1de029bb049fd15b8bc3e1e716ae3e9ec55b3b6f
    Author: Tarik Kirgin <hi@tarikkirgin.com>
    Date:   Tue Sep 3 03:17:49 2024 +0100
    
        add markdown plugins and styling
    
    commit e73c6da1e94ec684a3458895032a117f7e09fe66
    Author: Tarik Kirgin <hi@tarikkirgin.com>
    Date:   Tue Sep 3 01:01:46 2024 +0100
    
        change css to scss
    
    commit 1c4a7c08ddcf6ef7c87e5450ab52191a184cfe46
    Author: Tarik Kirgin <hi@tarikkirgin.com>
    Date:   Tue Sep 3 00:40:39 2024 +0100
    
        update project external link icon to match external link
    
    commit eed22762dc4235d9bd485602978f3cd1f5009e95
    Author: Tarik Kirgin <hi@tarikkirgin.com>
    Date:   Tue Sep 3 00:32:40 2024 +0100
    
        add external link styling
    
    commit 5c4e07c7d036ca998e2d9913191e31ef008995b4
    Author: Tarik Kirgin <hi@tarikkirgin.com>
    Date:   Tue Sep 3 00:32:28 2024 +0100
    
        refactor link styling
    
    commit e8d2426521eea56f6faa1ca8d6231c83043a8606
    Author: Tarik Kirgin <hi@tarikkirgin.com>
    Date:   Mon Sep 2 23:40:49 2024 +0100
    
        add blog content collection and basic layout
    
    commit df50aec5ce5c43bf8473410cdb958b364c670a87
    Author: Tarik Kirgin <hi@tarikkirgin.com>
    Date:   Mon Sep 2 23:38:29 2024 +0100
    
        refactor main layout
    
    commit 8ebc88420205a617c3c7c96796985f37ced7b147
    Author: Tarik Kirgin <hi@tarikkirgin.com>
    Date:   Mon Sep 2 23:37:21 2024 +0100
    
        adjust colors

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,47 @@ import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import react from "@astrojs/react";
 import vercel from "@astrojs/vercel/serverless";
+import mdx from "@astrojs/mdx";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import rehypeExternalLinks from "rehype-external-links";
+import rehypePrettyCode from "rehype-pretty-code";
+import rehypeSlug from "rehype-slug";
+import remarkBreaks from "remark-breaks";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), icon(), react()],
-  output: "server",
+  integrations: [
+    tailwind(),
+    icon(),
+    react(),
+    mdx({
+      syntaxHighlight: false,
+      remarkRehype: {
+        clobberPrefix: "",
+      },
+      remarkPlugins: [remarkBreaks],
+      rehypePlugins: [
+        rehypeSlug,
+        [
+          rehypeExternalLinks,
+          {
+            properties: {
+              class: "external-link",
+            },
+            target: "_blank",
+            rel: ["noopener noreferrer"],
+          },
+        ],
+        [
+          rehypePrettyCode,
+          {
+            theme: "monokai",
+          },
+        ],
+      ],
+    }),
+  ],
+  output: "hybrid",
   adapter: vercel({
     webAnalytics: {
       enabled: true,
---
+++ src/content/config.ts
@@ -0,0 +1,21 @@
+import { z, defineCollection } from "astro:content";
+
+const blogCollection = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    date: z.date(),
+    author: z.string(),
+    image: z
+      .object({
+        url: z.string(),
+        alt: z.string(),
+      })
+      .optional(),
+    tags: z.array(z.string()).optional(),
+  }),
+});
+
+export const collections = {
+  blog: blogCollection,
+};
